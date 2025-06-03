d92882b57cd358a146d0b6f6a288cb2a3ea5dbc2
itsblen <lamblaine@yahoo.com>
2025-06-03 16:01:45 -0700

    increased odds of getting gurked

--- packages/bot/src/commands/maple.ts
+++ packages/bot/src/commands/maple.ts
@@ -94,7 +94,7 @@ async function renderCharacter(
 	const canvas = createCanvas(600, 400);
 	const ctx = canvas.getContext('2d');
 	ctx.fillStyle = '#0000ff';
-	const asset = assets[Math.ceil(Math.random())];
+	const asset = assets[Math.random() < 0.001 ? 0:1];
 	const background = await loadImage(path.join(rootDir, asset.name));
 	ctx.drawImage(
 		background,
